c1e0615233f1b29751ea18f835c0848aba7ea037
simonsong03 <sson5747@tamu.edu>
2026-03-22 16:10:43 -0500

    Completed OWASP vulnerability site

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  console.log("Server received:", email, password);
+
+  if (!email || !password) {
+    return res.status(400).json({ 
+        message: "Email and password are required." });
+  }
+  if (!email.includes("@")) {
+    return res.status(400).json({ 
+        message: "Invalid email format." });
+  }
+  if (password.length < 8) {
+    return res.status(400).json({ 
+        message: "Password must be at least 8 characters long." });
+  }
+  res.json({ message: "Login input validated successfully." });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
